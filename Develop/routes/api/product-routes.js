6ff5c4f1dd6bf191098a2ddda04988ea526207ae
aidanespinosa <aidanespinosa3@gmail.com>
2023-01-17 12:24:10 -0800

    Wrote API routes and Database models

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,22 +1,51 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// find all products
+// be sure to include its associated Category and Tag data
+router.get("/", async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ["category_name"],
+        },
+        {
+          model: Tag,
+          attributes: ["tag_name"],
+        },
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// find a single product by its `id`
+// be sure to include its associated Category and Tag data
+router.get("/:id", async (req, res) => {
+  try {
+    const productData = await Product.findByPK(req.params.id, {
+      include: [{ model: Product, through: Category }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No product with that id" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -24,7 +53,8 @@ router.post('/', (req, res) => {
       stock: 3,
       tagIds: [1, 2, 3, 4]
     }
-  */
+*/
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -48,7 +78,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,8 +119,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({ where: { id: req.params.id } });
+
+    if (!productData) {
+      res.status(404).json({ message: "No project found with this id!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# E-commerce Back End Starter Code
+# Ecommerce Back End
+
+Generator for a employee profile generator using Node.js with the Node module Inquirer Package, and using Jest for running tests.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributors](#contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+
+  # Installation
+
+  Make sure to use the terminal and run npm install and run NPM run seed to sync up your MYSQL2 DataBase.
+
+## Usage
+
+Use Insomnia Core to Test all routes and make sure DataBase is synced properly.
+
+## License
+
+The MIT License
+
+## Contributors
+
+Aidan Espinosa
+
+## Tests
+
+Jest Node Module
+
+## Questions
+
+- Git Hub: https://github.com/aidanespinosa
+- Email: aidanespinosa3@gmail.com
+
+## Link to how-to-video
+
+https://drive.google.com/file/d/1qpBCiaoW_-xUXi1T8LhIYXOErpG5VgQk/view
